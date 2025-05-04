d534c9e7b55d87e40c55b3633f94b4633450e105
Shani-Schiff <s0534107188@gmail.com>
2025-05-04 18:23:29 +0300

    todos GET

--- server/routes/todosRoutes.js
+++ server/routes/todosRoutes.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const dbPromise = require("../dbConnection");
 const router = express.Router();
-router.get('/',);
-router.put(':id',);
+router.get('/', async (req, res) => {
+    const userId = req.query.userId;
+    db.query(`SELECT * FROM todos WHERE userId = ?`, [userId])
+        .then(([rows]) => {
+            if (rows.length === 0) {
+                return res.status(404).json({ message: 'Todos not found' });
+            }
+            res.status(200).json(rows);
+        })
+        .catch((err) => {
+            console.error('Error fetching todos:', err);
+            res.status(500).json({ error: 'Internal server error' });
+        });
+});
+router.put(':id', async (req, res) => {
+    const { id } = req.params;
+    const db = await dbPromise;
+    const { complete } = req.body;
+});
 router.post('/',);
 router.delete("/:id",);
 module.exports = router
