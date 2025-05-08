35db0e502f5832cda3aa023e2b6d68988a998f38
Shani-Schiff <s0534107188@gmail.com>
2025-05-08 22:47:29 +0300

    HomePageCss

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
 
   return (
     <div className='userName'>
-   <div><div className="animated-title">Welcome to  {user ? user.name : "Guest user"}'s Dashboard</div><h1 className="welcome-message">Welcome to our website!, your one-stop platform for managing todos, sharing posts, exploring albums, engaging with comments, and viewing user data! Whether you're staying organized, expressing your thoughts, or browsing memories, we've got you covered.</h1></div>
-    
+      <div><div className="animated-title">Welcome to  {user ? user.name : "Guest user"}'s Dashboard</div><h1 className="welcome-message">Welcome to our website! your one-stop platform for managing todos, sharing posts, exploring albums, engaging with comments, and viewing user data! Whether you're staying organized, expressing your thoughts, or browsing memories, we've got you covered.</h1></div>
+
     </div>
   );
 };
